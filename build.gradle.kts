eb4aeb3543e3790a01be09bde2ba48836d9cdb0c
Luke Duncan <luke.william.duncan@gmail.com>
2020-08-20 07:15:41 +0100

    Preserving structure of empty dirs

---
+++ build.gradle.kts
@@ -0,0 +1,35 @@
+buildscript {
+    repositories {
+        google()
+        gradlePluginPortal()
+    }
+    dependencies {
+        classpath("org.gradle.kotlin:plugins:1.3.6")
+    }
+}
+
+plugins {
+    `kotlin-dsl`
+    id("org.jetbrains.kotlin.jvm") version "1.3.72"
+    java
+}
+
+tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+}
+
+repositories {
+    jcenter()
+}
+
+dependencies {
+    implementation(platform("org.jetbrains.kotlin:kotlin-bom"))
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+
+    testImplementation("org.jetbrains.kotlin:kotlin-test")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit")
+
+}
+
